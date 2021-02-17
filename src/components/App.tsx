02145858d69b7a1390c5f8f1011b518bde5d6c5a
andriy1399 <andrijn333@gmail.com>
2021-02-17 16:23:50 +0200

    added json-server

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import '../styles/App.scss';
 
-function App() {
-  return (
-    <div className="app">
-      Project was created
-    </div>
-  );
-}
+const App: React.FC = () => {
+  return <div className="app">Project was created</div>;
+};
 
 export default App;
