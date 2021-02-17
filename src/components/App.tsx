25e5ef432fc678dcc255993dd10178e8e94ca228
andriy1399 <andrijn333@gmail.com>
2021-02-17 15:56:26 +0200

    created react-app

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      Project was created
+    </div>
+  );
+}
+
+export default App;
