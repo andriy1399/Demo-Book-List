0e22bc38f027d9e55759055f237b11a59ddb05bc
andriy1399 <andrijn333@gmail.com>
2021-02-18 19:43:34 +0200

    created select in form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -22,11 +22,30 @@ const renderInput = (
 ): JSX.Element => {
   const { input, label, id, meta } = props;
   const { touched, error } = meta;
-  const classes = `form-control ${error && touched ? 'is-invalid': ''}`
+  const classes = `form-control ${error && touched ? 'is-invalid' : ''}`;
   return (
     <div className="mb-3">
       <label htmlFor={id}>{label}</label>
       <input {...input} className={classes} id={id} autoComplete="off" />
+      <div className="invalid-feedback">{error}</div>
+    </div>
+  );
+};
+
+const renderSelect = (
+  props: FieldProps &
+    React.InputHTMLAttributes<HTMLInputElement> &
+    WrappedFieldProps
+): JSX.Element => {
+  const { input, meta, children } = props;
+  const { touched, error } = meta;
+  const classes = `form-select ${error && touched ? 'is-invalid' : ''}`;
+  return (
+    <div className=" mb-3">
+      <label htmlFor="category">Category</label>
+      <select className={classes} {...input} id="category">
+        {children}
+      </select>
       <div className="invalid-feedback">{error}</div>
     </div>
   );
@@ -37,7 +56,6 @@ const Form: React.FC<any> = props => {
 
   const submitForm = (book: any) => {
     props.onSubmit(book);
-    // eslint-disable-next-line react-hooks/rules-of-hooks
     history.push('/');
   };
   return (
@@ -59,8 +77,20 @@ const Form: React.FC<any> = props => {
           name="category"
           label="Category"
           id="category"
-          component={renderInput}
-        />
+          component={renderSelect}
+          defaultValue={'DEFAULT'}
+        >
+          <option value="DEFAULT" disabled>
+            Choose...
+          </option>
+          <option value="Classics">Classics</option>
+          <option value="Comic Book">Comic Book</option>
+          <option value="Fantasy">Fantasy</option>
+          <option value="Horror">Horror</option>
+          <option value="Literary Fiction">Literary Fiction</option>
+          <option value="Detective and Mystery">Detective and Mystery</option>
+        </Field>
+
         <Field name="ISBN" label="ISBN" id="ISBN" component={renderInput} />
 
         <button type="submit" className="btn btn-dark">
@@ -74,5 +104,5 @@ const Form: React.FC<any> = props => {
 export default reduxForm<IBook>({
   form: 'bookForm',
   validate,
-  enableReinitialize: true
+  enableReinitialize: true,
 })(Form);
